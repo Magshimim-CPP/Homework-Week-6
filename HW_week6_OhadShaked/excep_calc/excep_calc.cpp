e7f1c0c621330a4cd93472aa6bb631bbff146ed3
Ohad Shaked <ohad.saked@gmail.com>
2023-02-20 21:58:36 +0200

    update the fuck up already

--- HW_week6_OhadShaked/excep_calc/excep_calc.cpp
+++ HW_week6_OhadShaked/excep_calc/excep_calc.cpp
@@ -1,24 +1,85 @@
 #include <iostream>
-int add(int a, int b) {
-  return a + b;
+#include <exception>
+
+#define NOT_ALLOWED 8200
+#define ERROR_MESSAGE "\nThis user is not authorized to access 8200\nplease enter different numbers, or try to get clearance in 1 year.\n"
+
+class MyException : public std::exception
+{
+public:
+    virtual const char* what() const;
+};
+
+const char* MyException::what() const
+{
+    return ERROR_MESSAGE;
 }
 
-int  multiply(int a, int b) {
-  int sum = 0;
-  for(int i = 0; i < b; i++) {
-    sum = add(sum, a);
-  };
-  return sum;
+
+int add(int a, int b)
+{
+    if (a + b == NOT_ALLOWED || a == NOT_ALLOWED || b == NOT_ALLOWED)
+    {
+        throw MyException();
+    }
+    return a + b;
 }
 
-int  pow(int a, int b) {
-  int exponent = 1;
-  for(int i = 0; i < b; i++) {
-    exponent = multiply(exponent, a);
-  };
-  return exponent;
+int multiply(int a, int b)
+{
+    if (a == NOT_ALLOWED || b == NOT_ALLOWED)
+    {
+        throw MyException();
+    }
+
+    int sum = 0;
+
+    for(int i = 0; i < b; i++)
+    {
+        sum = add(sum, a);
+    };
+    return sum;
 }
 
-int main(void) {
-  std::cout << pow(5, 5) << std::endl;
+int pow(int a, int b)
+{
+    if (a == NOT_ALLOWED || b == NOT_ALLOWED)
+    {
+        throw MyException();
+    }
+
+    int exponent = 1;
+    for(int i = 0; i < b; i++)
+    {
+        exponent = multiply(exponent, a);
+    };
+    return exponent;
+}
+
+int main(void)
+{
+    try
+    {
+        std::cout << pow(3, 7) << std::endl; //OK;
+    }
+    catch (const MyException& error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    try
+    {
+        std::cout << multiply(4100, 2) << std::endl; //NOT ALLOWED
+    }
+    catch (const MyException& error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+    try
+    {
+        std::cout << add(8200, 2) << std::endl; //NOT ALLOWED
+    }
+    catch (const MyException& error)
+    {
+        std::cout << error.what() << std::endl;
+    }
 }
